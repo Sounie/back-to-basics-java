dbf9bd9dddf91322d008a5dae6d8e1dbb6e988af
sounie <Sounie@users.noreply.github.com>
2019-12-12 14:55:36 +0000

    Extending testing for more threads, with random delays per call.

--- src/test/java/sounie/ConcurrencyInMapTest.java
+++ src/test/java/sounie/ConcurrencyInMapTest.java
@@ -2,9 +2,12 @@ package sounie;
 
 import org.junit.Test;
 
+import java.security.SecureRandom;
+import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -43,5 +46,31 @@ public class ConcurrencyInMapTest {
         int result = wordMap.getCountForWord("foo");
 
         assertEquals(1000, result);
+    }
+
+    @Test
+    public void shouldHandleThirtyThreads() throws InterruptedException {
+        ConcurrencyInMap wordMap = new ConcurrencyInMap();
+
+        SecureRandom random = new SecureRandom();
+
+        ExecutorService executorService = Executors.newFixedThreadPool(30);
+
+        for (int i = 0; i < 10000; i++) {
+            executorService.submit(() -> {
+                wordMap.addWord("foo");
+                try {
+                    Thread.sleep(random.nextInt(15));
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+
+        executorService.awaitTermination(4, TimeUnit.SECONDS);
+
+        int result = wordMap.getCountForWord("foo");
+
+        assertEquals(10000, result);
     }
 }
